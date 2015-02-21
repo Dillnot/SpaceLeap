9316b75d81ed6403e71db4e54533910210624b6b
Dylan <stevensondylan13@googlemail.com>
2015-02-21 17:56:08 +0000

    main menu set-up

--- core/src/sapceleap/game/SpaceLeapGame.java
+++ core/src/sapceleap/game/SpaceLeapGame.java
@@ -1,63 +1,38 @@
 package sapceleap.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import spaceleap.engine.screen.MainMenuScreen;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SpaceLeapGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Sprite player;
+public class SpaceLeapGame extends Game {
 	
-	java.util.Random rand = new java.util.Random();
 	
+	
+	public SpriteBatch batch;
+	public BitmapFont font;
+
 	@Override
 	//Handles all the creation of objects, classes and other stuff in the game
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		player = new Sprite(img);
-		
-		player.setPosition(250, 250);
+		// Use LibGDX's default Arial font.
+		font = new BitmapFont();
+		this.setScreen(new MainMenuScreen(this));
 	}
 
-	@Override //Calls update and render one after another
+	 //Calls update and render one after another
 	public void render () {
-		update();
-		draw();
+		super.render();
+	}
+	
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
 	}
 	
 	
-	//This method will call all updates from within the game
-	private void update()
-	{
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                player.translateX(-1f);
-            else
-                player.translateX(-10.0f);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                player.translateX(1f);
-            else
-                player.translateX(10.0f);
-        }
-	}
-	
-	//This method will draw everything into the screen
-	private void draw()
-	{
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		player.draw(batch);
-		batch.end();
-	}
 	
 
 }
---
+++ core/src/spaceleap/engine/screen/GameScreen.java
@@ -0,0 +1,130 @@
+/**
+ * 
+ */
+package spaceleap.engine.screen;
+
+import sapceleap.game.SpaceLeapGame;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * @author dylan
+ *
+ */
+public class GameScreen implements Screen {
+
+	final SpaceLeapGame game;
+	private SpriteBatch batch;
+	private Texture img;
+	private Sprite player;
+
+	/**
+	 * 
+	 */
+	public GameScreen(SpaceLeapGame game) {
+		this.game = game;
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		player = new Sprite(img);
+		
+		player.setPosition(250, 250);
+	}
+
+	
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#show()
+	 */
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#render(float)
+	 */
+	@Override
+	public void render(float delta) {
+		update();
+		draw();
+
+	}
+
+	private void draw() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		player.draw(batch);
+		batch.end();
+		
+	}
+
+
+	private void update() {
+		 if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+	            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+	                player.translateX(-1f);
+	            else
+	                player.translateX(-10.0f);
+	        }
+	        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+	            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+	                player.translateX(1f);
+	            else
+	                player.translateX(10.0f);
+	        }
+	}
+
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#resize(int, int)
+	 */
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#pause()
+	 */
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#resume()
+	 */
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#hide()
+	 */
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#dispose()
+	 */
+	@Override
+	public void dispose() {
+		batch.dispose();
+		img.dispose();
+	}
+
+}
