748a3b74d2b54687b9a567d9439640addb0597dd
projectgoav <edm22@hotmail.co.uk>
2015-02-21 18:56:21 +0000

    Added player image and ability to read from Leap

--- core/src/sapceleap/game/SpaceLeapGame.java
+++ core/src/sapceleap/game/SpaceLeapGame.java
@@ -1,28 +1,35 @@
 package sapceleap.game;
 
+import spaceleap.engine.input.LeapListener;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.leapmotion.leap.Controller;
+
 
 public class SpaceLeapGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Sprite player;
 	
-	java.util.Random rand = new java.util.Random();
+	Controller c = new Controller();
+	LeapListener l = new LeapListener();
 	
 	@Override
 	//Handles all the creation of objects, classes and other stuff in the game
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("Player.png");
 		player = new Sprite(img);
 		
 		player.setPosition(250, 250);
+		
+        // Have the sample listener receive events from the controller
+        c.addListener(l);
 	}
 
 	@Override //Calls update and render one after another
@@ -35,7 +42,7 @@ public class SpaceLeapGame extends ApplicationAdapter {
 	//This method will call all updates from within the game
 	private void update()
 	{
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+        /*if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
             if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
                 player.translateX(-1f);
             else
@@ -46,7 +53,10 @@ public class SpaceLeapGame extends ApplicationAdapter {
                 player.translateX(1f);
             else
                 player.translateX(10.0f);
-        }
+        }*/
+		
+		player.setX(l.position.getX());
+       
 	}
 	
 	//This method will draw everything into the screen
@@ -58,6 +68,4 @@ public class SpaceLeapGame extends ApplicationAdapter {
 		player.draw(batch);
 		batch.end();
 	}
-	
-
 }
