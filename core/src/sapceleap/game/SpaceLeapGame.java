0c77e186055d9272c0c6f38f33b296b6fe7d1e50
projectgoav <edm22@hotmail.co.uk>
2015-02-21 16:20:28 +0000

    Eclipse integration complete and added random movement of sprite

--- core/src/sapceleap/game/SpaceLeapGame.java
+++ core/src/sapceleap/game/SpaceLeapGame.java
@@ -4,24 +4,49 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.particles.influencers.ColorInfluencer.Random;
 
 public class SpaceLeapGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Sprite player;
+	
+	java.util.Random rand = new java.util.Random();
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		player = new Sprite(img);
 	}
 
-	@Override
+	@Override //Calls update and render one after another
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		update();
+		draw();
+	}
+	
+	
+	//This method will call all updates from within the game
+	private void update()
+	{
+		int x = rand.nextInt(500);
+		int y = rand.nextInt(500);
+		
+		player.setPosition(x, y);
+	}
+	
+	//This method will draw everything into the screen
+	private void draw()
+	{
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		player.draw(batch);
 		batch.end();
 	}
+	
+
 }
